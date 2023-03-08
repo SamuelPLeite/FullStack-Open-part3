db33b9dfeb251ec8e47ccf0cdce1de415a7e7147
Samuel Leite <shpl19@inf.ufpr.br>
2023-03-08 17:33:58 -0300

    Exercise 3.10-3.11.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Phone Book
+
+Answers to the exercises related to the Phone Book application from course "Full stack open - part 3" (https://fullstackopen.com/en/part3).
+
+Deployed to: https://phonebook-cagy.onrender.com/api/persons
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('data', (req, res) => JSON.stringify(req.body))
 const app = express()
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny', {
     skip: (req, res) => req.method === 'POST'
