518658e8e5e25c941950efa78879136d3c0dc416
Samuel Leite <shpl19@inf.ufpr.br>
2023-03-08 16:48:01 -0300

    Exercises 3.9.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny', {
     skip: (req, res) => req.method === 'POST'
