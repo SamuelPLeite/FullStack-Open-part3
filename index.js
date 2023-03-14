4ac047bc95c510258d97a9b21158bb948b133fea
Samuel Leite <shpl19@inf.ufpr.br>
2023-03-14 14:38:29 -0300

    Exercises 3.13-3.14.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 morgan.token('data', (req, res) => JSON.stringify(req.body))
 
@@ -42,17 +44,18 @@ app.get('/info', (request, response) =>
 )
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person)
-        response.json(person)
-    else
-        response.status(404).end()
+    Person.findById(request.params.id).then(person => {
+        if (person)
+            response.json(person)
+        else
+            response.status(404).end()
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -74,22 +77,22 @@ app.post('/api/persons/', (request, response) => {
         })
 
 
-    if (persons.some(person => person.name === body.name))
+    /* if (persons.some(person => person.name === body.name))
         return response.status(400).json({
             error: "Name already in the database."
-        })
+        }) */
 
-    const person = {
-        id: Math.floor(Math.random() * 1000000),
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(person)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
