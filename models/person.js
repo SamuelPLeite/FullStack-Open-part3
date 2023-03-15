2e8242d58c2b7af3ab405c30af16c4102f3259d7
Samuel Leite <shpl19@inf.ufpr.br>
2023-03-15 16:59:19 -0300

    Exercise 3.22.

--- models/person.js
+++ models/person.js
@@ -7,41 +7,41 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('Connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 const numberValidator = (number) => {
-    const exp = new RegExp('^\\d{2,3}-?\\d{4,}$')
-    return exp.test(number)
+  const exp = new RegExp('^\\d{2,3}-?\\d{4,}$')
+  return exp.test(number)
 }
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        validate: {
-            validator: numberValidator,
-            message: (props) => `number ${props.value} format is incorrect.`
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: numberValidator,
+      message: (props) => `number ${props.value} format is incorrect.`
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -1,47 +1,47 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url =
-    `mongodb+srv://thesamhpl:${password}@clusterpart3.u1vuplh.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://thesamhpl:${password}@clusterpart3.u1vuplh.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
-const Person = mongoose.model("Person", personSchema)
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    console.log('Phonebook:')
-    Person.find({}).then(persons => {
-        persons.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  console.log('Phonebook:')
+  Person.find({}).then(persons => {
+    persons.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 4) {
-    console.log('Missing phone number!')
-    process.exit(1)
+  console.log('Missing phone number!')
+  process.exit(1)
 } else if (process.argv.length === 5) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
 
-    person.save().then(result => {
-        console.log(`added ${person.name} number ${person.number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(result => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-    console.log('Too many arguments! Make sure the name is enclosed in quotes')
-    process.exit(1)
+  console.log('Too many arguments! Make sure the name is enclosed in quotes')
+  process.exit(1)
 }
